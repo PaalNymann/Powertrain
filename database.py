d78eb4621bdcd8a8b9b1b80663e4542ef0233634
Paal Nymann <nyman@macbook-pro.lan>
2025-08-12 14:01:15 +0200

    Fix missing func import in database.py

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime, Boolean, or_
+from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime, Boolean, or_, func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
